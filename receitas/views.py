b2a25503d91110df0755e3f7b310b7b29c0cbf68
ElHopperCBR <cesar.brandao@sp.senai.br>
2025-10-24 20:49:40 -0300

    Criando pastas separadas, não está funcionando

--- receitas/views.py
+++ receitas/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render (request, 'home.html', context={
-        'nome': 'Receitas Django'
+    return render (request, 'pages/home.html', context={
+        'nome': 'Receitas com Django'
     })
