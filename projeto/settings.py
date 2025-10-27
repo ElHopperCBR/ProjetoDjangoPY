86f811238e69361307baa45513f760d54c7d5a37
ElHopperCBR <cesar.brandao@sp.senai.br>
2025-10-27 18:13:37 -0300

    Consertado

--- projeto/settings.py
+++ projeto/settings.py
@@ -56,8 +56,7 @@ ROOT_URLCONF = 'projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [    
-        ],
+        'DIRS': [          ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
